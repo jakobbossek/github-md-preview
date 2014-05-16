92c56492bec7530c08393a406ae39e4b6561a986
Jakob Bossek <info@jakobbossek.de>
2014-05-16 12:50:57 +0200

    considerably improve code block language detection (now guessing lexer if no language given)

--- markdown/test.md
+++ markdown/test.md
@@ -4,7 +4,7 @@ Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
 tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
 vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
 no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-consetetur sadipscing elitr, sed **diam nonumy eirmod tempor invidunt** ut labore et 
+consetetur sadipscing elitr, sed **diam nonumy eirmod tempor invidunt** ut labore et
 dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
 duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
 Lorem ipsum dolor sit amet.
@@ -24,4 +24,11 @@ def fac(n):
     f = f * n
     n = n - 1
   return f
+```
+
+Another code block without explicit language
+```
+library(soobench)
+fn = generate_ackley_function(2)
+plot(fn)
 ```
--- preview.py
+++ preview.py
@@ -20,22 +20,35 @@ def getMarkdownFiles(source_dir = "."):
   return file_list
 
 def highlight_codeblocks(sourcecode):
-  #FIXME: this is so unbelievably ugly
-  subs = re.findall(r"```[a-zA-z]+\s.*```", sourcecode, re.S)
+  subs = re.findall(r"```[a-zA-z]*\s.*?```", sourcecode, re.S)
   if (len(subs) == 0):
     return sourcecode
-  subs = subs[0].replace("```", "")
+
   #print(subs)
-  lang = re.findall(r"^[a-zA-z]+\s", subs)[0]
-  lang = lang[0:(len(lang)-1)]
-  lexer = get_lexer_by_name(lang)
-  #print(lexer)
+
   def replace_codeblock(codeblock):
-    codeblock = codeblock.group(0) # get entire match as string
+    block = codeblock.group(0)
+    block = block.replace("```", "")
+    codeblock = codeblock.group(0)
     codeblock = re.sub(r"```.*(\s)?", "", codeblock)
+
+    # determine language
+    lang = re.findall(r"^[a-zA-z]+\s", block)
+
+    lexer = None
+    if (len(lang) == 0):
+      #print("No explicit language found. Guessing lexer!")
+      lexer = guess_lexer(codeblock)
+    else:
+      lang = lang[0]
+      lang = lang[0:(len(lang)-1)]
+      lexer = get_lexer_by_name(lang)
+    #print(lexer)
+
     return highlight(codeblock, lexer, HtmlFormatter())
 
-  regex = re.compile(r"```[a-zA-Z]+\s.*?```", re.S)
+  # replace markdown source code blocks with HTML markup
+  regex = re.compile(r"```[a-zA-z]*\s.*?```", re.S)
   sourcecode = re.sub(regex, replace_codeblock, sourcecode)
   return sourcecode
 
